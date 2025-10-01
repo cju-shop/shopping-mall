b49627e14a4e7c3d30ab8bbd689589d9100eb614
yujin3613 <dldbwls7115@naver.com>
2025-10-01 23:09:04 +0900

    추천 상품 그리드 제작

---
+++ src/main/java/com/cju/shoppingmall/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.cju.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Map<String, Object>> products = new ArrayList<>();
+
+        for (int i = 1; i <= 20; i++) {
+            Map<String, Object> p = new HashMap<>();
+            p.put("id", i);
+            p.put("name", "라이트 니트 카디건 #" + i);
+            p.put("price", 39900 + (i % 5) * 1000);
+            p.put("img", "https://picsum.photos/seed/" + i + "/600/450"); // 👈 키 이름 = img
+            p.put("freeShipping", i % 3 == 0);
+            products.add(p);
+        }
+
+        model.addAttribute("products", products); // 👈 꼭 추가
+        return "screens/home";
+    }
+
+}

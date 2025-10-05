ea7187b4b8c0cf85054f7f13ebfcaabb407fd98c
yujongyeop <230233@naver.com>
2025-10-05 13:46:32 +0900

    feat: JpaAuditing 활성화
    [개요]
    - 엔티티 변경 감지를 위해 JpaAuditing을 활성화 시킨다.
    
    [변경 사항]
    - @EnableAuditing 어노테이션 추가

--- src/main/java/com/cju/shoppingmall/ShoppingmallApplication.java
+++ src/main/java/com/cju/shoppingmall/ShoppingmallApplication.java
@@ -2,12 +2,14 @@ package com.cju.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShoppingmallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingmallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallApplication.class, args);
+    }
 
 }

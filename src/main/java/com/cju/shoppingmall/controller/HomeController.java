0134150bd12338e93a4e0984d9f52f5f4e1227fd
Platform1117 <230233@naver.com>
2025-10-27 19:01:56 +0900

    feat: 최신 상품 DB 조회식으로 변경
    [개요]
    - Object 타입으로 반환하던 상품 정보를 데이터베이스에 불러와 반환하는 방식으로 변경
    - 최신 상품 영역에 대해서만 진행
    
    [변경 사항]
    - _card에서 컬럼명과 맞지 않은 부분 수정
    - /create/home-data URL 접속 시 데이터 생성 후 / 경로로 리다이렉트 하도록 설정(데이터 삽입을 위한 용도)
    - GET '/' 요청 시 productService로부터 최근 등록된 상품을 받아오도록 변경

--- src/main/java/com/cju/shoppingmall/controller/HomeController.java
+++ src/main/java/com/cju/shoppingmall/controller/HomeController.java
@@ -1,83 +1,87 @@
 package com.cju.shoppingmall.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cju.shoppingmall.member.entity.Member;
+import com.cju.shoppingmall.member.entity.MemberRole;
+import com.cju.shoppingmall.member.service.MemberService;
+import com.cju.shoppingmall.product.entity.Category;
+import com.cju.shoppingmall.product.entity.Product;
+import com.cju.shoppingmall.product.service.ProductService;
+
 @Controller
 public class HomeController {
+
+    private final ProductService productService;
+    private final MemberService memberService;
+
+    public HomeController(ProductService productService, MemberService memberService) {
+        this.productService = productService;
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/create/home-data")
+    public String prepareHomeData() {
+        Member createdMember = memberService.join(
+            new Member("admin", "관리자", "관리자", "noEncrypted",
+                "01000000000", "admin@shopping.com", MemberRole.ADMIN));
+        Category category = productService.createCategory(new Category(null, "상품", true));
+        for (int i = 0; i < 10; i++) {
+            String productName = "Test Item " + i;
+            String description = "Test item 입니다.";
+            String thumbnail = "https://picsum.photos/seed/" + i + "/600/450";
+            Product generatedProduct = new Product(productName, description, thumbnail, 10000L, category,
+                createdMember);
+            productService.create(generatedProduct);
+        }
+        return "redirect:/";
+    }
+
     @GetMapping("/")
     public String home(Model model) {
-        List<Map<String, Object>> todayRecommendProducts = new ArrayList<>();
+        List<Product> dailyRecommentProducts = productService.getNewProducts();
         List<Map<String, Object>> bestProducts = new ArrayList<>();
         List<Map<String, Object>> newProducts = new ArrayList<>();
 
-        for (int i = 1; i <= 4; i++) {
-            Map<String, Object> p = new HashMap<>();
-            p.put("id", i);
-            p.put("name", "라이트 니트 카디건 #" + i);
-            p.put("price", 39900 + (i % 5) * 1000);
-            p.put("img", "https://picsum.photos/seed/" + i + "/600/450");
-            todayRecommendProducts.add(p);
-        }
         for (int i = 1; i <= 8; i++) {
             Map<String, Object> p = new HashMap<>();
             p.put("id", i);
             p.put("name", "라이트 니트 카디건 #" + i);
-            p.put("price", 39900 + (i % 5) * 1000);
-            p.put("img", "https://picsum.photos/seed/" + i + "/600/450");
+            p.put("basePrice", 39900 + (i % 5) * 1000);
+            p.put("thumbnail", "https://picsum.photos/seed/" + i + "/600/450");
             bestProducts.add(p);
         }
         for (int i = 1; i <= 8; i++) {
             Map<String, Object> p = new HashMap<>();
             p.put("id", i);
             p.put("name", "라이트 니트 카디건 #" + i);
-            p.put("price", 39900 + (i % 5) * 1000);
-            p.put("img", "https://picsum.photos/seed/" + i + "/600/450");
+            p.put("basePrice", 39900 + (i % 5) * 1000);
+            p.put("thumbnail", "https://picsum.photos/seed/" + i + "/600/450");
             newProducts.add(p);
         }
 
         List<Map<String, String>> banners = new ArrayList<>();
-        banners.add(Map.of(
-                "img", "/img/ad1.jpg",
-                "href", "https://example.com/ad1",
-                "alt", "제휴사 광고 1"
-        ));
-        banners.add(Map.of(
-                "img", "/img/ad2.jpg",
-                "href", "https://example.com/ad2",
-                "alt", "제휴사 광고 2"
-        ));
-        banners.add(Map.of(
-                "img", "/img/ad3.jpg",
-                "href", "https://example.com/ad3",
-                "alt", "제휴사 광고 3"
-        ));
+        banners.add(Map.of("img", "/img/ad1.jpg", "href", "https://example.com/ad1", "alt", "제휴사 광고 1"));
+        banners.add(Map.of("img", "/img/ad2.jpg", "href", "https://example.com/ad2", "alt", "제휴사 광고 2"));
+        banners.add(Map.of("img", "/img/ad3.jpg", "href", "https://example.com/ad3", "alt", "제휴사 광고 3"));
 
         //제품광고(ad) 리스트
         List<Map<String, String>> ads = new ArrayList<>();
-        ads.add(Map.of(
-                "img", "/img/ad1.jpg",
-                "href", "https://example.com/productAd1",
-                "alt", "제품 광고 1"));
-        ads.add(Map.of(
-                "img", "/img/ad2.jpg",
-                "href", "https://example.com/productAd2",
-                "alt", "제품 광고 2"));
-        ads.add(Map.of(
-                "img", "/img/ad3.jpg",
-                "href", "https://example.com/productAd3",
-                "alt", "제품 광고 2"));
+        ads.add(Map.of("img", "/img/ad1.jpg", "href", "https://example.com/productAd1", "alt", "제품 광고 1"));
+        ads.add(Map.of("img", "/img/ad2.jpg", "href", "https://example.com/productAd2", "alt", "제품 광고 2"));
+        ads.add(Map.of("img", "/img/ad3.jpg", "href", "https://example.com/productAd3", "alt", "제품 광고 2"));
 
         model.addAttribute("banners", banners);
         model.addAttribute("ads", ads);
 
-        model.addAttribute("todayRecommendProducts", todayRecommendProducts);
+        model.addAttribute("todayRecommendProducts", dailyRecommentProducts);
         model.addAttribute("bestProducts", bestProducts);
         model.addAttribute("newProducts", newProducts);
         return "screens/home";

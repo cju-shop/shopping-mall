ca2f021fac27f9b20844c2a3721cd36e6b925ffc
Platform1117 <230233@naver.com>
2025-10-27 14:52:04 +0900

    feat: 엔티티 리스너 및 생성자 추가
    [개요]
    - @CreatedDate와 @LastModifiedDate 정상 동작을 위한 엔티티 리스너 추가
    - 기본 생성자와 생성자 추가
    
    [변경 사항]
    - 각 엔티티에 AuditingEntityListener를 추가
    - 기본 생성자 추가(protected 로 제한)
      - JPA가 필요해서 생성
    - 생성자 추가

--- src/main/java/com/cju/shoppingmall/member/entity/Member.java
+++ src/main/java/com/cju/shoppingmall/member/entity/Member.java
@@ -4,9 +4,13 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,7 +18,9 @@ import lombok.Getter;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,6 +36,7 @@ public class Member {
     private String phone;
     @Column(nullable = false, unique = true)
     private String email;
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private MemberRole role;
     @CreatedDate
@@ -37,4 +44,19 @@ public class Member {
     private LocalDateTime createdAt;
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    public Member(String username, String nickname, String name, String password, String phone, String email,
+        MemberRole role) {
+        this.username = username;
+        this.nickname = nickname;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+        this.role = role;
+    }
+
+    protected Member() {
+
+    }
 }

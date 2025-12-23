1cdc7558facce516e823ab20be95e27f51968ecc
yujin <dldbwls7115@naver.com>
2025-12-24 00:02:20 +0900

    fix: ProductVariantOption 저장되지 않던 복합키 매핑 문제 수정

--- src/main/java/com/cju/shoppingmall/product/entity/ProductVariantOption.java
+++ src/main/java/com/cju/shoppingmall/product/entity/ProductVariantOption.java
@@ -1,13 +1,9 @@
 package com.cju.shoppingmall.product.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "product_variant_option")
 @IdClass(ProductVariantOptionId.class)
 public class ProductVariantOption {
     @Id
